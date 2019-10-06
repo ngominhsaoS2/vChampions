a2ce10ccd6d49a229df7d43437f388a2c9ce4447
saonm <ngominhsaoS2@gmail.com>
2019-10-07 04:10:14 +0700

    Add validateClub by using fastet-validator

--- vChampions.API/models/club.js
+++ vChampions.API/models/club.js
@@ -1,7 +1,15 @@
-const Joi = require('joi');
 const mongoose = require('mongoose');
+let Validator = require("fastest-validator");
+let v = new Validator();
 
 const clubSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+    unique: true
+  },
   name: {
     type: String,
     required: true,
@@ -14,72 +22,72 @@ const clubSchema = new mongoose.Schema({
   },
   captain: {
     type: new mongoose.Schema({
-        name: {
-            type: String,
-            required: true,
-            minlength: 5,
-            maxlength: 50
-        },
-        email: {
-            type: String,
-            required: true,
-            minlength: 5,
-            maxlength: 255,
-            unique: true
-        },
-        phone: {
-            type: String,
-            required: true,
-            minlength: 10,
-            maxlength: 12,
-            unique: true
-        },
-        avatar: {
-            imgId: { type: String, default: '' },
-            imgVersion: { type: String, default: '' }
-        },
-        description: {
-            type: String,
-            required: false,
-            minlength: 5,
-            maxlength: 200
-        }
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      },
+      email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 12,
+        unique: true
+      },
+      avatar: {
+        imgId: { type: String, default: '' },
+        imgVersion: { type: String, default: '' }
+      },
+      description: {
+        type: String,
+        required: false,
+        minlength: 5,
+        maxlength: 200
+      }
     })
   },
   players: [
     {
-        type: new mongoose.Schema({
-            name: {
-                type: String,
-                required: true,
-                minlength: 5,
-                maxlength: 50
-            },
-            email: {
-                type: String,
-                required: true,
-                minlength: 5,
-                maxlength: 255,
-                unique: true
-            },
-            phone: {
-                type: String,
-                required: true,
-                minlength: 10,
-                maxlength: 12,
-                unique: true
-            },
-            avatar: {
-                imgId: { type: String, default: '' },
-                imgVersion: { type: String, default: '' }
-            },
-            description: {
-                type: String,
-                required: false,
-                minlength: 5,
-                maxlength: 200
-            }
-        })
+      type: new mongoose.Schema({
+        name: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50
+        },
+        email: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 255,
+          unique: true
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 10,
+          maxlength: 12,
+          unique: true
+        },
+        avatar: {
+          imgId: { type: String, default: '' },
+          imgVersion: { type: String, default: '' }
+        },
+        description: {
+          type: String,
+          required: false,
+          minlength: 5,
+          maxlength: 200
+        }
+      })
     }
   ]
 });
@@ -88,13 +96,13 @@ const Club = mongoose.model('Club', clubSchema);
 
 function validateClub(club) {
   const schema = {
-    name: Joi.string().min(3).max(50).required(),
-    captainId: Joi.objectId().required(),
+    code: { type: "string", empty: false, min: 3, max: 50, pattern: /^\S*$/ }, // string phải theo đúng RegEx (không chứa dấu phẩy)
+    name: { type: "string", empty: false, min: 3, max: 50 } // empty: false - tức là không cho trường code empty
   };
 
-  return Joi.validate(club, schema);
+  return v.validate(club, schema)
 }
 
 exports.clubSchema = clubSchema;
-exports.Club = Club; 
+exports.Club = Club;
 exports.validateClub = validateClub;
--- vChampions.API/startup/routes.js
+++ vChampions.API/startup/routes.js
@@ -4,6 +4,7 @@ const express = require('express');
 const auth = require('../routes/auth');
 const users = require('../routes/users');
 const roles = require('../routes/roles');
+const clubs = require('../routes/clubs');
 
 // middlewares
 const error = require('../middlewares/error');
@@ -13,6 +14,7 @@ module.exports = function (app) {
     app.use('/api/auth', auth);
     app.use('/api/roles', roles);
     app.use('/api/users', users);
+    app.use('/api/clubs', clubs);
 
     // SaoNM this error logging middleware is registered after all routes middlewares
     app.use(error);
