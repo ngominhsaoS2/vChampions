e2b08e5644802afe58b8381073c08913cbcdbd90
saonm <ngominhsaoS2@gmail.com>
2019-10-26 18:30:28 +0700

    Add carousel component

--- vChampions-SPA/src/app/app.module.ts
+++ vChampions-SPA/src/app/app.module.ts
@@ -1,25 +1,38 @@
+// Built-in modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDropdownModule, CarouselModule  } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
+// Customized modules
+import { appRoutes } from './routes';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      NavComponent
+      NavComponent,
+      RegisterComponent,
+      HomeComponent,
+      CarouselComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      CarouselModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [],
    bootstrap: [
