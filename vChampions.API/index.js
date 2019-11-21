f5fe74beeb7dc41f9457d05202187b91de1e098a
saonm <ngominhsaoS2@gmail.com>
2019-11-21 08:01:29 +0700

    Fix bug can not uploade big image

--- vChampions.API/index.js
+++ vChampions.API/index.js
@@ -2,6 +2,9 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
+app.use(express.json({ limit: '50mb' })); // dùng để nhận json từ request
+app.use(express.urlencoded({ extended: true, limit: '50mb' })); // dùng cho x-www-form-urlencoded tại postman
+
 require('./startup/cors-config')(app);
 require('./startup/logging');
 require('./startup/routes')(app);
